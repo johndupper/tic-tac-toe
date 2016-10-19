c0942f735e85f6aacf7115cf8e21daef02e32236
John Dupper <john.dupper@me.com>
2016-10-19 15:17:51 -0400

    added color click to each square element

--- app.js
+++ app.js
@@ -1,17 +1,23 @@
+// allow alerts:
+/*jslint devel: true */
+
+
+
+
 // create game
 var gameContainer = document.getElementById('board');
-
-function alertMe() {
-    'use strict';
-}
-
 var createBoard = function () {
     'use strict';
     var index, square;
     for (index = 0; index < 9; index += 1) {
         square = document.createElement('div');
         square.className = 'tile';
-        square.addEventListener('click', alertMe);
+
+        // tile events
+        square.addEventListener('click', function () {
+            this.style.backgroundColor = 'yellow';
+        });
+
         gameContainer.appendChild(square);
     }
 };
