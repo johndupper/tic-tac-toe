71a6a6fd0def0f7495d8ed7570499a3b2f3b42e5
John Dupper <john.dupper@me.com>
2016-10-19 19:18:43 -0400

    if a square is selected, it cannot be overwritten to change the value of it

--- app.js
+++ app.js
@@ -1,19 +1,42 @@
+// set counter
+var playerTurn = 0;
 
+
+
+function checkForWinner() {
+    'use strict';
+}
+
+
+// assign colors per turn
 function ChangeColor() {
     'use strict';
-    this.style.backgroundColor = 'yellow';
+    
+    if (this.classList.contains('free')) {
+        this.classList.remove('free');
+        
+        if (playerTurn % 2 === 0) {
+            this.style.backgroundColor = 'blue';
+        } else {
+            this.style.backgroundColor = 'red';
+        }
+        
+        playerTurn += 1;
+    }
 }
 
 
 // create game
 var gameContainer = document.getElementById('board');
+
 var createBoard = function () {
     'use strict';
+
     var index, square;
     for (index = 0; index < 9; index += 1) {
         square = document.createElement('div');
-        square.className = 'tile';
-        // tile events
+        square.className = 'tile free';
+        square.id = index + 1;
         square.addEventListener('click', ChangeColor);
         gameContainer.appendChild(square);
     }
