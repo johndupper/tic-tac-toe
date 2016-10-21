73d594c7fb56342eba562b1dba49f84f8caed32a
John Dupper <john.dupper@me.com>
2016-10-20 23:17:23 -0400

    game fully functional. reset feature complete so game can be played over and over without refreshing the page

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+// variables
 var playerTurn = 0;
 var playerOne = [];
 var playerTwo = [];
@@ -6,31 +7,51 @@ var wins = [[1, 2, 3], [4, 5, 6], [7, 8, 9],
             [1, 5, 9], [3, 5, 7]];
 var alert;
 
-// win or tie
-function declareWinner(blueArray, redArray) {
+// reset game
+function resetGame() {
     'use strict';
-    var index;
+    var index, resetSquares;
+    resetSquares = document.getElementsByClassName('tile');
+    
+    for (index = 0; index < resetSquares.length; index += 1) {
+        resetSquares[index].className = 'tile free';
+    }
+    
+    playerTurn = 0;
+    playerOne = [];
+    playerTwo = [];
+}
+
+// win or tie
+function declareWinner() {
+    'use strict';
+    var index, playerOneCount, indexPlayerOne, playerTwoCount, indexPlayerTwo;
     
     for (index = 0; index < wins.length; index += 1) {
-        
-        var playerOneCount = 0
-        for (var indexPlayerOne = 0; indexPlayerOne < playerOne.length; indexPlayerOne += 1) {
+
+        playerOneCount = 0;
+        for (indexPlayerOne = 0; indexPlayerOne < playerOne.length; indexPlayerOne += 1) {
             if (wins[index].indexOf(playerOne[indexPlayerOne]) > -1) {
-                playerOneCount++;
+                playerOneCount += 1;
             }
         }
 
-        var playerTwoCount = 0
-        for (var indexPlayerTwo = 0; indexPlayerTwo < playerTwo.length; indexPlayerTwo += 1) {
+        playerTwoCount = 0;
+        for (indexPlayerTwo = 0; indexPlayerTwo < playerTwo.length; indexPlayerTwo += 1) {
             if (wins[index].indexOf(playerTwo[indexPlayerTwo]) > -1) {
-                arrayCount++;
+                playerTwoCount += 1;
             }
         }
 
-        if (playerOneCount == 3 || playerTwoCount == 3) {
-            alert('winner');
-            break;
+        if (playerOneCount === 3 || playerTwoCount === 3) {
+            alert('You won! Congrats!');
+            resetGame();
         }
+    }
+    
+    if (playerTurn === 9) {
+        alert('We have a tie! Try again?');
+        resetGame();
     }
 }
 
@@ -38,17 +59,16 @@ function declareWinner(blueArray, redArray) {
 function GameLogic() {
     'use strict';
     var chosenSquare;
-    
     chosenSquare = parseInt(this.id, 10);
     
     if (this.classList.contains('free')) {
         this.classList.remove('free');
-        
+
         if (playerTurn % 2 === 0) {
-            this.style.backgroundColor = 'blue';
+            this.classList.add('blue');
             playerOne.push(chosenSquare);
         } else {
-            this.style.backgroundColor = 'red';
+            this.classList.add('red');
             playerTwo.push(chosenSquare);
         }
         
@@ -57,7 +77,7 @@ function GameLogic() {
     }
     
     if (playerTurn >= 5) {
-        declareWinner(playerOne.sort(), playerTwo.sort());
+        declareWinner();
     }
 }
 
